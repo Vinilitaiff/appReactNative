aa39bcb0f8a23795d416bde81b764842a080cc8e
vinilitaiff <vinilitaiff@gmail.com>
2019-08-05 23:38:51 -0400

    impl viewUser e listagem de starred com estilizacao

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 import {
   Container,
   Form,
@@ -15,14 +18,38 @@ import {
   ProfileButtonText,
 } from './styles';
 
-import api from '../../services/api';
-
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuarios',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     newUser: '',
     users: [],
     loading: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
     const { newUser, users } = this.state;
@@ -46,6 +73,12 @@ export default class Main extends Component {
     Keyboard.dismiss();
   };
 
+  handleNavigation = user => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  };
+
   render() {
     const { users, loading } = this.state;
     return (
@@ -59,7 +92,7 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+          <SubmitButton loading={loading} onPress={() => this.handleAddUser}>
             {loading ? (
               <ActivityIndicator color="#FFF" />
             ) : (
@@ -77,7 +110,7 @@ export default class Main extends Component {
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigation(item)}>
                 <ProfileButtonText>Ver Perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -87,7 +120,3 @@ export default class Main extends Component {
     );
   }
 }
-
-Main.navigationOptions = {
-  title: 'Usuarios',
-};
